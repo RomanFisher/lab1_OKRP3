5afeb97f00029db647da5fc3336ba0db1de6a0e2
RomanFisher <romaribak59@gmail.com>
2022-10-05 11:49:14 +0300

    добавив метод сортування Шейкер

--- lab1/Sort.cs
+++ lab1/Sort.cs
@@ -13,12 +13,48 @@ namespace lab1
     {
         public static Address[] bubble(Address[] address, MethodInfo method)
         {
-            
+            bool needSort;
+            Address tempAddress;
+
+            do
+            {
+                needSort = false;
+                for (int i = 0; i < address.Length - 1; i++)
+                    if (Comparer.DefaultInvariant.Compare(method.Invoke(address[i], null), method.Invoke(address[i + 1], null)) == 1)
+                    {
+                        tempAddress = address[i];
+                        address[i] = address[i + 1];
+                        address[i + 1] = tempAddress;
+                        needSort = true;
+                    }
+            } while (needSort);
             return address;
         }
         public static Address[] mix(Address[] address, MethodInfo method)
         {
-            
+            bool needSort;
+            Address tempAddress;
+
+            do
+            {
+                needSort = false;
+                for (int i = 0; i < address.Length - 1; i++)
+                    if (Comparer.DefaultInvariant.Compare(method.Invoke(address[i], null), method.Invoke(address[i + 1], null)) == 1)
+                    {
+                        tempAddress = address[i];
+                        address[i] = address[i + 1];
+                        address[i + 1] = tempAddress;
+                        needSort = true;
+                    }
+                for (int i = address.Length - 1; i > 0; i--)
+                    if (Comparer.DefaultInvariant.Compare(method.Invoke(address[i], null), method.Invoke(address[i - 1], null)) == -1)
+                    {
+                        tempAddress = address[i];
+                        address[i] = address[i - 1];
+                        address[i - 1] = tempAddress;
+                        needSort = true;
+                    }
+            } while (needSort);
             return address;
         }
         public static Address[] MinimumElement(Address[] address, MethodInfo method)
